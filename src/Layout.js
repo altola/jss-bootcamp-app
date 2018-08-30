780de69c978f800f7caaf5f323fb74a82194c7c1
Alex Shyba <alex@altola.co>
2018-08-30 15:48:02 -0700

    barebones navbar

--- src/Layout.js
+++ src/Layout.js
@@ -3,6 +3,7 @@ import { Placeholder } from '@sitecore-jss/sitecore-jss-react';
 import { NavLink } from 'react-router-dom';
 import { translate } from 'react-i18next';
 import Helmet from 'react-helmet';
+import Navigation from './components/Nagivation';
 
 // Using bootstrap is completely optional. It's used here to provide a clean layout for samples,
 // without needing extra CSS in the sample app. Remove it in package.json as well if it's removed here.
@@ -17,31 +18,6 @@ import logo from './assets/jss-logo.png';
   All routes share this root layout by default (this could be customized in RouteHandler),
   but components added to inner placeholders are route-specific.
 */
-
-let Navigation = ({ t, i18n }) => (
-  <div className="nav d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 border-bottom">
-    <h5 className="my-0 mr-md-auto font-weight-normal">
-      <NavLink to="/">
-        <img src={logo} height={50} alt="Sitecore" />
-      </NavLink>
-    </h5>
-    <nav className="my-2 my-md-0 mr-md-3">
-      <a
-        className="p-2"
-        href="https://jss.sitecore.net"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        {t('Documentation')}
-      </a>
-    </nav>
-  </div>
-);
-
-// inject dictionary props (`t`) into navigation so we can translate it
-// NOTE: using this is needed instead of using i18next directly to keep
-// the component state updated when i18n state (e.g. current language) changes
-Navigation = translate()(Navigation);
 
 const Layout = ({ route, context }) => {
   return (
---
+++ src/components/Nagivation/index.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem } from 'reactstrap';
+
+class Navigation extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+  render() {
+    return (
+      <div>
+        <Navbar color="light" light expand="md">
+          <NavbarBrand href="/">Sitecore Rocks</NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+    );
+  }
+}
+
+export default Navigation;
