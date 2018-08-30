2a737d5c4f557b96412507011331df8c2a8d7b97
Alex Shyba <alex@altola.co>
2018-08-30 12:56:08 -0700

    one more param

--- src/components/Jumbo/index.js
+++ src/components/Jumbo/index.js
@@ -2,19 +2,9 @@ import React from "react";
 import { RichText, Text, Link } from "@sitecore-jss/sitecore-jss-react";
 import { Jumbotron, Button } from "reactstrap";
 
-// const Jumbo = props => (
-//   <Jumbotron>
-//     <Text tag="h1" className="display-3" field={props.fields.title} />
-//     <RichText field={props.fields.text} />
-//     <p className="lead">
-//       <Link className="btn btn-primary" field={props.fields.callToAction} />
-//     </p>
-//   </Jumbotron>
-// );
-
 const Jumbo = ({fields, params}) => (
   <Jumbotron>
-    <Text tag="h1" className="display-3" field={fields.title} />
+    <Text tag="h1" className={params.titleClass} field={fields.title} />
     <RichText field={fields.text} />
     <p className={params.containerClass}>
       <Link className={params.linkClass} field={fields.callToAction} />
